5487bf878acc51d326021ee031bc61c79510f46f
unknown <biswasmahmuda5@gmail.com>
2023-11-27 09:57:32 -0800

    iterate of keys of values and key

--- milestone.js
+++ milestone.js
@@ -24,9 +24,69 @@ const number = parseInt(num);
 // console.log(++x);
 
 // if else condition
-let age = 5;
-if (age >= 18) {
-  console.log("you are adult");
-} else {
-  console.log("tumi bacha acho");
-}
+// let age = 5;
+// if (age >= 18) {
+//   console.log("you are adult");
+// } else {
+//   console.log("tumi bacha acho");
+// }
+
+//  switch
+
+// let mark = 10;
+// switch (mark) {
+//   case 10:
+//     console.log("you got fail");
+//     break;
+//   case 40:
+//     console.log("you got A-");
+//     break;
+//   case 60:
+//     console.log("you got A");
+//     break;
+//   default:
+//     console.log("you got a");
+// }
+
+// const arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+// for (let i = 0; i <= arr.length; i++) {
+//   if (arr[i] === 30) {
+//     break;
+//   }
+//   console.log(arr[i]);
+// }
+
+// const raining = true;
+// const umbrella = false;
+
+// if (raining === true && umbrella === true) {
+//   console.log("bye re jabo");
+// } else {
+//   console.log("bye re jabo na");
+// }
+
+// const pepole = 276;
+// const peopleForCar = pepole % 50;
+// const peopleForRiksha = peopleForCar % 11;
+// const totalVara = peopleForRiksha * 70;
+// console.log(totalVara);
+
+// const person = {
+//   name: "liam",
+//   age: 20,
+//   class: "twelve",
+//   place: "sworuppur",
+// };
+// const keys = Object.keys(person);
+// const values = Object.values(person);
+// console.log(values);
+// console.log(keys);
+
+// for (let i = 0; i <= keys.length; i++) {
+//   console.log(person[keys[i]]);
+// }
+
+// for (let key of keys) {
+//   console.log(person[key]);
+// }
